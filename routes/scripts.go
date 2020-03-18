35be9ef6202bb419d315649a2c401a3c04ec8c7c
ThomasLee94 <thomaslee9474@gmail.com>
2020-03-18 14:43:45 -0700

    [add] macos routes

--- routes/scripts.go
+++ routes/scripts.go
@@ -25,3 +25,24 @@ func GetUbuntuCustom(app *echo.Echo) {
 func GetUbuntuFile(app *echo.Echo) {
 	app.GET("/api/v1/os/ubuntu/scripts/:uuid", controllers.GetUbuntuFile)
 }
+
+/* ----------------------------- MacOS Packages ------------------------- */
+
+func GetMacOSGeneric(app *echo.Echo) {
+	app.GET("/api/v1/os/macos/generic", controllers.GetMacOSGeneric)
+}
+
+// returns object of supported downloads
+func GetMacOSItems(app *echo.Echo) {
+	app.GET("/api/v1/os/macos/availableItems", controllers.GetMacOSItems)
+}
+
+// send arr's of software to setup, returns a custom setup script
+func GetMacOSCustom(app *echo.Echo) {
+	app.POST("/api/v1/osmacosu/dynamic", controllers.GetMacOSCustom)
+}
+
+// returns file when user runs setup script from terminal
+func GetMacOSFile(app *echo.Echo) {
+	app.GET("/api/v1/os/macos/scripts/:uuid", controllers.GetMacOSFile)
+}
