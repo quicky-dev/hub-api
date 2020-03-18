4b7b53e3403fdce5cc6e5c904397d8c1a9dccd54
ThomasLee94 <thomaslee9474@gmail.com>
2020-03-18 14:35:51 -0700

    [add] ubuntu script routes

--- routes/scripts.go
+++ routes/scripts.go
@@ -5,6 +5,22 @@ import (
 	"github.com/quicky-dev/hub-api/controllers"
 )
 
+/* ----------------------------- Linux Packages ----------------------------- */
+
+func GetUbuntuGeneric(app *echo.Echo) {
+	app.GET("/api/v1/os/ubuntu/generic", controllers.GetUbuntuGeneric)
+}
+
+// returns object of supported downloads
+func GetUbuntuItems(app *echo.Echo) {
+	app.GET("/api/v1/os/ubuntu/availableItems", controllers.GetUbuntuItems)
+}
+
+// send arr's of software to setup, returns a custom setup script
+func GetUbuntuCustom(app *echo.Echo) {
+	app.POST("/api/v1/os/ubuntu/dynamic", controllers.GetUbuntuCustom)
+}
+
 // returns file when user runs setup script from terminal
 func GetUbuntuFile(app *echo.Echo) {
 	app.GET("/api/v1/os/ubuntu/scripts/:uuid", controllers.GetUbuntuFile)
