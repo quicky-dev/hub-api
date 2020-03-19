b494830ed0691380821d008210a233a903cf45ea
ThomasLee94 <thomaslee9474@gmail.com>
2020-03-19 13:33:44 -0700

    [add] route registers

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/labstack/echo"
+import (
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/quicky-dev/hub-api/routers"
+)
 
 func main() {
-	e := echo.New()
+	app := echo.New()
+
+	routers.RegisterScriptsRoutes(app)
+
+	app.Logger.Fatal(app.Start(os.Getenv("PORT")))
 }
