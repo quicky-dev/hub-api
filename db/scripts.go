eacab47c74bb8be6972160001b1d5671f16727d3
ThomasLee94 <thomaslee9474@gmail.com>
2020-03-20 20:45:55 -0700

    [add] GetScripts

--- db/scripts.go
+++ db/scripts.go
@@ -7,7 +7,9 @@ import (
 	"log"
 
 	"github.com/quicky-dev/hub-api/util"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var script = util.MongoClient.Database("Hub").Collection("Script")
@@ -21,7 +23,8 @@ type Script struct {
 	Comments  []primitive.ObjectID
 }
 
-// CreateScript creates a new empty Script
+// CreateScript creates a new empty Script, population of Script attributes will have to be
+// done seperately.
 func (this *Script) SaveScript() (string, error) {
 	insertion, err := script.InsertOne(context.TODO(), this)
 
@@ -41,8 +44,33 @@ func (this *Script) SaveScript() (string, error) {
 }
 
 // GetScripts returns a list of all Scripts
-func (this *Script) GetScripts() []Script {
+func (this *Script) GetScripts() error {
+	findOptions := options.Find()
+	empty_filter := bson.D{{}}
 
+	var results []*Script
+
+	find, err := script.Find(context.TODO(), empty_filter, findOptions)
+
+	// if multiple documents are found, a cursor will be returned
+	// iterate through cursor
+	for find.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var elem Script
+		err := find.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &elem)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
 }
 
 func (this *Script) DeleteScript(id int64) string {
