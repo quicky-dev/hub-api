10ef154f67107e84176156d171bf2f185765e7a6
C3NZ <cenz@cenz.io>
2020-03-23 21:36:26 -0700

    [add] save functionality for the comments

--- db/comments.go
+++ db/comments.go
@@ -1,7 +1,36 @@
 package db
 
-type Comments struct {
-	comment  string
-	username string
-	auth_id  string
+import (
+	"context"
+	"errors"
+	"github.com/quicky-dev/auth-service/util"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+)
+
+var comments = util.MongoClient.Database("Hub").Collection("Comments")
+
+type Comment struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Content  string             `bson:"content"`
+	Username string             `bson:"username"`
+	AuthID   string             `bson:"authID"`
+}
+
+func (this *Comment) Save() error {
+	insertion, err := comments.InsertOne(context.TODO(), this)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	ID, status := insertion.InsertedID.(primitive.ObjectID)
+
+	if status == false {
+		return errors.New("Couldn't save the comment.")
+	}
+
+	this.ID = ID
+	return nil
 }
