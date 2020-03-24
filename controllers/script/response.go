43473ee957cbb9632b1071a249e19a49b3699321
ThomasLee94 <thomaslee9474@gmail.com>
2020-03-23 23:56:52 -0700

    [add] get script response

--- controllers/script/response.go
+++ controllers/script/response.go
@@ -1,5 +1,13 @@
 package script
 
+import (
+	"github.com/quicky-dev/hub-api/db"
+)
+
 type scriptError struct {
 	ErrorMsg string `json:"errorMessage"`
+}
+
+type getScriptResponse struct {
+	Script db.Script `json:"script"`
 }
